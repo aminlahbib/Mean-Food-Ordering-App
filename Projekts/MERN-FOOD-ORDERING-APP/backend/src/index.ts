c4f39e72db5d521cdc19b2f311d34edc95ab3615
aminlahbib <amine.lhb00@gmail.com>
2024-09-24 21:16:11 +0200

    setting up backend files

---
+++ Projekts/MERN-FOOD-ORDERING-APP/backend/src/index.ts
@@ -0,0 +1,15 @@
+import  express, {Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();                                                  /* create a new express server and assign it to express var */
+app.use(express.json());                                                /* auto convert the body to any request we make to our api server to json */
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response)=>{                 /* when the backend gets a request it gives a response saying hello */                    
+    res.json({ message: "Hello!"});
+});
+
+app.listen(5500, ()=> {
+    console.log("server started on localhost:5500");
+})
